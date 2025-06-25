15c431a6371db0eeb90bde9e7986556651d022db
貴央 <your-email@example.com>
2025-06-25 22:23:48 +0900

    最初のコミットです！

---
+++ record-commit.sh
@@ -0,0 +1,7 @@
+git init => このフォルダ内の変更履歴を、Git っていう記録システムで管理できるようにする
+git add  => 変更されたファイルを記録対象に追加する
+git commit -m ""=> 履歴にスナップショットとして記録
+git push => 送信
+git remote set-url origin https://github.com/(アカウント名)/(レポジトリ名).git => githubに繋ぐ
+git push -u origin master => アカウント名を入力=>トークンを入力
+git push --set-upstream origin master => ブランチの送信先を「origin/master 」に設定
