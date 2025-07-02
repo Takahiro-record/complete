8077bdc388fecd5214518e0f01a2dd065538d86c
貴央 <your-email@example.com>
2025-07-02 16:02:32 +0900

    ログ自動更新(2025-07-02 16:02)

---
+++ python_excel.py
@@ -0,0 +1,42 @@
+from openpyxl import load_workbook
+
+wb = load_workbook("logbook.xlsx")
+ws = wb["Log"]
+
+#------1行目を全部表示
+for row in ws.iter_rows(min_row=1,max_row=1,values_only=True):
+    print(row)
+
+#------3列目だけを全部取り出してリストにする
+messages = []
+for row in ws.iter_rows(min_row=2,values_only=True)
+    message.append(row[2])
+
+print(messages)
+
+#------空じゃないカテゴリだけ出力
+for row in ws.iterrows(min_row=2,values_only=True):
+    ifrow[1]:
+    print(row[1])
+
+#------カテゴリが"業務”の行だけ内容を表示
+for row in ws.iter_rows(min_row=2,values_only=True):
+    if row[1] == "業務":
+        print(row[2])
+
+#------日時と内容をセットで表示
+for row in ws.iter_rows(min_row=2,values_only=True):
+    print(f"{row[0]}:{row[2]}")
+
+#------内容に"git"が含まれる行だけ表示
+for row in ws.iter_rows(min_row=2,values_only=True):
+    if row[2]and "Git" in row[2]
+        print(row[2])
+
+#------カテゴリの種類を全部表示
+categories = set()
+for row in ws.iter_rows(min_row=2,values_only=True):
+    if row[1]:
+        categories.aappend([row[1]])
+
+len(categories)
--- rensyu.py
+++ rensyu.py
@@ -1,7 +1,25 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
+
+wb_new = Workbook()
+ws_kyukei = wb_new.active
+ws_kyukei.title="休憩"
+ws_gyomu = wb_new.create_sheet(title="業務")
+ws_gakusyu = wb_new.create_sheet(title="学習")
+
 
 wb = load_workbook("logbook.xlsx")
 ws = wb["Log"]
 
+ws_kyukei.append(["日付","内容"])
+ws_gyomu.append(["日付","内容"])
+ws_gakusyu.append(["日付","内容"])
+
 for row in ws.iter_rows(min_row=2,values_only=True):
-    print(row[2])
+    if row[1] == "学習" and row[2]:
+        ws_gakusyu.append([row[0],row[2]])
+    elif row[1] == "業務" and row[2]:
+        ws_gyomu.append([row[0],row[2]])
+    elif row[1] == "休憩" and row[2]:
+        ws_kyukei.append([row[0],row[2]])
+
+wb_new.save("categorized_log.xlsx")
